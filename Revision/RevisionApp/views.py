e726b9c00d46cb371cab0e885d66497c5dc7add3
Robert Ndungu Njane <robert.ndungu@hotmail.com>
2020-08-20 15:48:21 +0300

    Add register and login UI Pages

--- Revision/RevisionApp/views.py
+++ Revision/RevisionApp/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+
+class Register(View):
+    def get(self, request):
+        return render(request, 'auth/register.html')
+
+    def post(self, request):
+        return render(request, 'auth/register.html')
+
+
+class Login(View):
+    def get(self, request):
+        return render(request, 'auth/login.html')
+
+    def post(self, request):
+        return render(request, 'auth/login.html')
